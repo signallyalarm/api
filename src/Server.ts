902363262d937f08b607d30d1c08af08a8249dc2
Lukas Germerott <lukas@lukasgermerott.de>
2020-02-18 20:53:58 +0100

    First commit

---
+++ src/Server.ts
@@ -0,0 +1,27 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import { Request, Response } from 'express';
+import logger from 'morgan';
+import path from 'path';
+import BaseRouter from './routes';
+
+// Init express
+const app = express();
+
+// Add middleware/settings/routes to express.
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use('/api', BaseRouter);
+
+/**
+ * Point express to the 'views' directory. If you're using a
+ * single-page-application framework like react or angular
+ * which has its own development server, you might want to
+ * configure this to only serve the index file while in
+ * production mode.
+ */
+
+// Export express instance
+export default app;
